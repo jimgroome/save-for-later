503a3a75b33d71958e02a71f8a011116970deeac
Jim Groome <james@jgroome.com>
2021-03-14 12:07:23 +0000

    New Settings modal, with logout button

---
+++ client/src/components/Settings.js
@@ -0,0 +1,31 @@
+import { MDBBtn, MDBModal, MDBModalBody, MDBModalFooter, MDBModalHeader } from "mdbreact";
+import React from "react";
+
+const Settings = ({ settingsModalOpen, setSettingsModalOpen, onSettingsSave, onLogoutClick }) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onSettingsSave();
+  };
+  return (
+    <MDBModal isOpen={settingsModalOpen} toggle={setSettingsModalOpen}>
+      <MDBModalHeader>Settings</MDBModalHeader>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <MDBModalBody>
+          <MDBBtn color="danger" onClick={(e) => onLogoutClick(e)}>
+            Sign outw
+          </MDBBtn>
+        </MDBModalBody>
+        <MDBModalFooter>
+          <MDBBtn onClick={() => setSettingsModalOpen(!settingsModalOpen)} color="secondary">
+            Cancel
+          </MDBBtn>
+          <MDBBtn type="submit" onClick={(e) => onSubmit(e)} color="primary">
+            Save
+          </MDBBtn>
+        </MDBModalFooter>
+      </form>
+    </MDBModal>
+  );
+};
+
+export default Settings;
